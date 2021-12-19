890dbecd41dca0e8014154aebcdcfba8a94ace60
Pavel Arkharov <parkharo@c1r1p11.hive.fi>
2021-12-19 19:18:21 +0200

    Working version of the project

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: parkharo <parkharo@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/22 21:38:02 by parkharo          #+#    #+#             */
+/*   Updated: 2021/12/19 19:17:49 by parkharo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft.h"
+
+# define BUFF_SIZE 32
+# define MAX_FD 12288
+
+int	get_next_line(const int fd, char **line);
+
+#endif
--- get_next_line2/eval_tests/testfiles/advanced_3
+++
@@ -1,3 +0,0 @@
-AAAA
-BBBB
-CCCC
